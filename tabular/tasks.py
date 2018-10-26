5bfeb61ca3d69c03ecdf2d57597e7098fee86bc8
Navin Ayer <ayernavin@togglecorp.com>
2018-10-26 14:46:41 +0545

    Add XLSX tabular extractor

--- tabular/tasks.py
+++ tabular/tasks.py
@@ -6,7 +6,7 @@ from redis_store import redis
 from django.db import transaction
 
 from .models import Book
-from .extractor import csv
+from .extractor import csv, xlsx
 
 
 logger = logging.getLogger(__name__)
@@ -15,12 +15,20 @@ logger = logging.getLogger(__name__)
 def _tabular_extract_book(book):
     if book.file_type == Book.CSV:
         csv.extract(book)
+    if book.file_type == Book.XLSX:
+        xlsx.extract(book)
+    return True
+
+
+def _tabular_meta_extract_book(book):
+    if book.file_type == Book.XLSX:
+        xlsx.extract_meta(book)
     return True
 
 
 @shared_task
 def tabular_extract_book(book_id):
-    key = 'tabular_file_extraction_{}'.format(book_id)
+    key = 'tabular_extract_book_{}'.format(book_id)
     lock = redis.get_lock(key, 60 * 60 * 24)  # Lock lifetime 24 hours
     have_lock = lock.acquire(blocking=False)
     if not have_lock:
@@ -34,6 +42,31 @@ def tabular_extract_book(book_id):
     except Exception:
         logger.error(traceback.format_exc())
         book.status = Book.FAILED
+        book.error = Book.UNKNOWN_ERROR  # TODO: handle all type of error
+        return_value = False
+
+    book.save()
+
+    lock.release()
+    return return_value
+
+
+@shared_task
+def tabular_meta_extract_book(book_id):
+    key = 'tabular_meta_extract_book_{}'.format(book_id)
+    lock = redis.get_lock(key, 60 * 60 * 24)  # Lock lifetime 24 hours
+    have_lock = lock.acquire(blocking=False)
+    if not have_lock:
+        return False
+
+    book = Book.objects.get(id=book_id)
+    try:
+        with transaction.atomic():
+            return_value = _tabular_meta_extract_book(book)
+        book.meta_status = Book.SUCCESS
+    except Exception:
+        logger.error(traceback.format_exc())
+        book.meta_status = Book.FAILED
         book.error = Book.UNKNOWN_ERROR  # TODO: handle all type of error
         return_value = False
 
